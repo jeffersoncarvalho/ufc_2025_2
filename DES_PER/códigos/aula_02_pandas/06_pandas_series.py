89c57ee59c5983967d91cc5eb101b1a84cea63e7
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-09-22 09:34:05 -0300

    pandas series alunos 4

--- DES_PER/códigos/aula_02_pandas/06_pandas_series.py
+++ DES_PER/códigos/aula_02_pandas/06_pandas_series.py
@@ -8,3 +8,7 @@ Dica: para filtrar uma Series use: "nome_series <operador> valor" como
 índice do colchetes da series original (nome_series)
 
 """
+import pandas as pd
+notas = pd.Series([9.3, 7.4, 5.6], index=["Matemática", "Biologia", "Geografia"]) 
+notas = notas[notas > 7]
+print(notas)
