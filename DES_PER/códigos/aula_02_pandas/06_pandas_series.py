5c0b0bf26256415db18542d75a467e9e02085852
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-09-22 09:24:45 -0300

    pandas series alunos 3

---
+++ DES_PER/códigos/aula_02_pandas/06_pandas_series.py
@@ -0,0 +1,10 @@
+"""
+Filtragem de dados
+
+Use a mesma Series do 05_pandas_series.py, imprimindo apenas as notas que
+são maiores que 7.0
+
+Dica: para filtrar uma Series use: "nome_series <operador> valor" como 
+índice do colchetes da series original (nome_series)
+
+"""
