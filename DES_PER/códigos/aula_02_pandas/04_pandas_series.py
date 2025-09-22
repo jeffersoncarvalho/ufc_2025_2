d600b8907947612c47c2a27d61fdcc0aeb3dd955
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-09-22 09:00:16 -0300

    pandas series alunos

---
+++ DES_PER/códigos/aula_02_pandas/04_pandas_series.py
@@ -0,0 +1,13 @@
+"""
+Exercício 01
+
+Crie uma Series chamada preco_frutas com o preço de três frutas:
+
+Maça: 2.50
+Banana: 5.60
+Abacate: 6.30
+
+Imprima o preço da Banana usando a chave. 
+Depois, imprima o mesmo preço, usando o series.iloc[pos] 
+
+"""
