280622acfd8a038b1c44203e79832c4a4f80b2c5
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-09-23 09:29:53 -0300

    dataframe

--- DES_PER/códigos/aula_03_pandas/04_pandas_dataframe.py
+++ DES_PER/códigos/aula_03_pandas/04_pandas_dataframe.py
@@ -10,8 +10,10 @@ alunos_df = pd.read_csv("./arquivos/alunos.csv")
 def situacao_aluno(nota):
     if nota > 7:
         return "Aprovado"
-    else:
+    elif nota < 4:
         return "Reprovado"
+    else:
+        return "Recuperação"
 
 alunos_df["Situação"] = alunos_df["Nota"].apply(situacao_aluno)
 print(alunos_df)
---
+++ DES_PER/códigos/aula_03_pandas/05_pandas_dataframe.py
@@ -0,0 +1,13 @@
+"""
+Crie um DataFrame com os seguintes dados:
+
+Nome	Idade	Cidade
+Ana	    23	    São Paulo
+Bruno	30	    Rio de Janeiro
+Carlos	27	    Curitiba
+Diana	22	    Belo Horizonte
+
+- Exibir apenas a coluna "Nome"
+- Exibir as colunas "Nome" e "Cidade"
+
+"""
