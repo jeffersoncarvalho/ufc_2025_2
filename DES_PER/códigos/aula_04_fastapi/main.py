429f19808edb182ab9ff34054929aedc6212d8a7
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-09-29 10:26:39 -0300

    ativ

--- DES_PER/códigos/aula_04_fastapi/main.py
+++ DES_PER/códigos/aula_04_fastapi/main.py
@@ -5,7 +5,18 @@ import pandas as pd
 #import time
 #import asyncio
 
+contador_id = 1
+alunos_df = pd.DataFrame([])
+
 class Aluno(BaseModel):
     nome: str
     curso: str
     IRA: float
+
+def criar_aluno(aluno: Aluno):
+    novo = {
+        "id": contador_id
+        "nome": aluno.nome,
+        "curso": aluno.curso,
+        "IRA": aluno.IRA
+    }
