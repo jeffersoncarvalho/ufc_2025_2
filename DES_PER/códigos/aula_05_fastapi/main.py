fdc56ea9369c3cc6cd096807482474b3cec51bbf
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-09-30 08:59:07 -0300

    fastapi alunos

---
+++ DES_PER/códigos/aula_05_fastapi/main.py
@@ -0,0 +1,44 @@
+# pip install fastapi uvicorn pandas
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import pandas as pd
+#import time
+#import asyncio
+
+#inciar o aplicativo (API)
+app = FastAPI()
+
+contador_id = 1
+alunos_df = pd.DataFrame(columns = ["id","nome","curso","IRA"])
+
+#modelo para a entidade Aluno
+class Aluno(BaseModel):
+    nome: str
+    curso: str
+    IRA: float
+
+#serviço de criação de um Aluno
+@app.post("/alunos")
+def criar_aluno(aluno: Aluno):
+    novo_aluno = {
+        "id": contador_id,
+        "nome": aluno.nome,
+        "curso": aluno.curso,
+        "IRA": aluno.IRA
+    }
+    #forma com o DataFrame._append
+    alunos_df = alunos_df._append(novo_aluno, ignore_index = True)
+    #forma com o concat
+    #alunos_df = pd.concat([alunos_df, pd.DataFrame([novo_aluno])], ignore_index = True)
+    contador_id = contador_id + 1
+    return {
+        "mensagem": "Aluno criado com sucesso!",
+        "aluno": novo_aluno
+    }
+
+#serviço de listagem de TODOS os alunos
+@app.get("/alunos")
+def listar_alunos():
+    return alunos_df.to_dict(orient = "records")
+
+
