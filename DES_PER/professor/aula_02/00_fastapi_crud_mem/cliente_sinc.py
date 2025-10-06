0a6dcad900a31e739b2193c05286ed871cd6d7c8
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-10-06 15:57:00 -0300

    finalizando a api

--- DES_PER/professor/aula_02/00_fastapi_crud_mem/cliente_sinc.py
+++ DES_PER/professor/aula_02/00_fastapi_crud_mem/cliente_sinc.py
@@ -11,9 +11,28 @@ def listar_alunos():
     resp = httpx.get(f"{BASE_URL}/alunos")
     print(resp.json())
 
-inicio = time.time()
-criar_aluno()
-criar_aluno()
-listar_alunos()
-fim = time.time()
-print("Diferença: ", (fim - inicio))
+def obter_aluno(id):
+    resp = httpx.get(f"{BASE_URL}/alunos/{id}")
+    print(resp.json())
+
+def atualizar_aluno(id, aluno):
+    resp = httpx.put(
+            f"{BASE_URL}/alunos/{id}",
+            json={"nome": aluno.get("nome"), "curso": aluno.get("curso"), "nota": aluno.get("nota")}
+        )
+    print(resp.json().get("aluno"))
+
+def apagar_aluno(id):
+    resp = httpx.delete(f"{BASE_URL}/alunos/{id}")
+    print(resp.json())
+
+#obter_aluno(2)
+#atualizar_aluno(1, {"nome": "TESTE", "curso": "TESTE CURSO", "nota": 0})
+apagar_aluno(2)
+
+#inicio = time.time()
+#criar_aluno()
+#criar_aluno()
+#listar_alunos()
+#fim = time.time()
+#print("Diferença: ", (fim - inicio))
--- DES_PER/professor/aula_02/00_fastapi_crud_mem/main.py
+++ DES_PER/professor/aula_02/00_fastapi_crud_mem/main.py
@@ -13,7 +13,15 @@ lock = asyncio.Lock()
 # iniciando o applicativo
 app = FastAPI()
 
-df = pd.DataFrame(columns=["id","nome","curso","nota"])
+#df = pd.DataFrame(columns=["id","nome","curso","nota"])
+df = pd.DataFrame(
+    {
+        "id" : [1,2,3,4],
+        "nome" : ["Jefferson", "Fábio", "Wladimir", "Marcelo"],
+        "curso" : ["SI", "DD", "ES", "SI"],
+        "nota" : [5.4, 6.7, 8.1, 9.7] 
+    }
+)
 contador_id = 1
 
 class Aluno(BaseModel):
@@ -23,6 +31,8 @@ class Aluno(BaseModel):
     nota: float
 
 # rota para criar o aluno
+# cria um lock para ser acessado por uma única thread por vez
+# sleep para simular tempo computacional
 @app.post("/alunos")
 async def criar_aluno(aluno: Aluno):
     async with lock:
@@ -50,4 +60,37 @@ async def criar_aluno(aluno: Aluno):
 async def listar_alunos():
     await asyncio.sleep(3)
     return df.to_dict(orient="records")
+
+#rota para obter aluno por id
+@app.get("/alunos/{id}")
+def obter_aluno(id: int):
+    global df
+    aluno =  df[df["id"]==id]
+    if aluno.empty:
+        raise HTTPException(status_code=404, detail="Aluno não encontrado")
+    return aluno.to_dict(orient = "records")[0]
+
+#rota para atualizar um aluno por id
+@app.put("/alunos/{id}")
+def atualizar_aluno(id: int, aluno: Aluno):
+    global df
+    aluno_antigo_idx = df.index[df["id"] == id]
+    if aluno_antigo_idx.empty:
+        raise HTTPException(status_code=404, detail="Aluno não encontrado")
+    df.loc[aluno_antigo_idx, ["nome", "curso", "nota"]] = [aluno.nome, aluno.curso, aluno.nota]
+    return {
+        "mensagem": f"Aluno id {id} atualizado com sucesso!",
+        "aluno": df.loc[aluno_antigo_idx].to_dict(orient="records")[0]
+    }
+
+#rota para apagar o aluno via id
+@app.delete("/alunos/{id}")
+def apagar_aluno(id: int):
+    global df
+    aluno_antigo_idx = df.index[df["id"] == id]
+    if aluno_antigo_idx.empty:
+        raise HTTPException(status_code=404, detail="Aluno não encontrado")
+    df = df.drop(aluno_antigo_idx).reset_index(drop=True)
+    return {"mensagem": f"Aluno com id {id} apagado com sucesso"}
+
 
