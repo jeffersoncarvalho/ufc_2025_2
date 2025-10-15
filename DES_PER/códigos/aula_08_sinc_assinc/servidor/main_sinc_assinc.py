746b8b1b8b676b7e23a04682b1f017e60b990cdd
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-10-15 09:36:44 -0300

    css

--- DES_PER/códigos/aula_08_sinc_assinc/servidor/main_sinc_assinc.py
+++ DES_PER/códigos/aula_08_sinc_assinc/servidor/main_sinc_assinc.py
@@ -1,8 +1,22 @@
 #uvicorn main_sinc_assinc:app --reload
 from fastapi import FastAPI
+import time
+import asyncio
 
 app = FastAPI()
 
 @app.get("/sinc")
+# trabalhar com dados em memória/arquivo, cálculos grandes, ou bases de dados blocantes, use 
+# rotas sincronas
 def rota_sincrona():
+    # operação blocante
+    time.sleep(2)
     return {"tipo": "SÍNCRONA"}
+
+# trabalhar com chamadas assíncronas dentro da API, como por exempo, httpx,
+# acessar uma base de dados com asyncpg ou simulando com o asyncio
+@app.get("/assinc")
+async def rota_assincrona():
+    # simulando uma chamada assincrona!
+    await asyncio.sleep(2) 
+    return {"tipo": "ASSÍNCRONA"}
