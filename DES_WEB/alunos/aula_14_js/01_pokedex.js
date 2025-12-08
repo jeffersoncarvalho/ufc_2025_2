f3966d7e1de0115d5838257982c5b42d02daa13e
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-12-08 10:41:26 -0300

    novo projeto pokedex

--- DES_WEB/alunos/aula_14_js/01_pokedex.js
+++ DES_WEB/alunos/aula_14_js/01_pokedex.js
@@ -1,11 +1,9 @@
 async function carregarPokemons() {
     const response = await fetch("http://localhost:3000/api/pokemons/listarTodos")
     const pokemons = await response.json()
-       
+    const pokemonContainerDiv = document.getElementById("pokemon-container") 
     pokemons.forEach(
-        (pokemon) => {
-            console.log(pokemon)
-        }
+        
     )
     
 }
