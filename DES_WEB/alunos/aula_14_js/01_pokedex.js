a474abb01301abe211f58ad104ae6314b250c0bd
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-12-08 11:28:05 -0300

    novo projeto pokedex

--- DES_WEB/alunos/aula_14_js/01_pokedex.js
+++ DES_WEB/alunos/aula_14_js/01_pokedex.js
@@ -15,12 +15,41 @@ async function carregarPokemons() {
                                     src= '${pokemon.imagem}'
                                     />`
             pokemonCardDivHTML += `<div class="site-card-buttons">
-                <button onclick="btn_voltar()">Capturar</button>
+                <button onclick='capturarPokemon(${JSON.stringify(pokemon)})'>Capturar</button>
                 </div>`
             pokemonCardDiv.innerHTML = pokemonCardDivHTML
             pokemonContainerDiv.appendChild(pokemonCardDiv)
         }
     )
+    
+}
+
+function capturarPokemon(pokemon){
+    //CUIDADO. Ao retirar de uma sessão, você está pegando uma
+    //String!
+    let pokemons = sessionStorage.getItem("pokemons")
+    if (pokemons) {
+        //salva o novo pokemon
+        pokemons = JSON.parse(pokemons)
+        //testar se eu já não o capturei antes!
+        const capturado = pokemons.some(elem => elem.id == pokemon.id)
+        if(!capturado) {
+            pokemons.push(pokemon)
+            sessionStorage.setItem("pokemons",JSON.stringify(pokemons))
+            console.log(`Pokemon ${pokemon.nome} adicionado!`)
+        }else {
+            alert(`Pokemon ${pokemon.nome} já capturado!`)
+        }
+        
+    }else {
+        //não existe o objeto que representa a lista de pokemons
+        //crie um novo
+        pokemons = []
+        pokemons.push(pokemon)
+        sessionStorage.setItem("pokemons", JSON.stringify(pokemons))
+        console.log("Sessão 'pokemons' vazia. Criando uma nova!")
+        console.log(`Adicionando ${pokemon.nome} à nova sessão.`)
+    }
     
 }
 
