164328fe3a1a9bb325b4082f50daf5c09e267977
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2025-12-09 11:18:25 -0300

    router

--- PIDM/turma_02/api-turma-02/routers/PokemonRouter.js
+++ PIDM/turma_02/api-turma-02/routers/PokemonRouter.js
@@ -1,2 +1,20 @@
 //responsável em transformar serviço (Service)
 //em rota acessível pelo cliente (browser, app web ou app mobile)
+const express = require("express")
+const pokemonService = require("../services/PokemonService")
+
+const router = express.Router()
+
+router.get(
+    "/listarPokemons",
+    async (request, response) => {
+        try{
+            const pokemonsDtos = await pokemonService.listarTodos()
+            response.json(pokemonsDtos)
+        }catch(error) {
+            console.log(error)
+            response.status(500).json({"erro":"Erro no servidor!"})
+        }
+    }
+)
+module.exports = router
